4d94271540186ea5fde934c480e5d9e8ba5260ea
JunsoMin <minjs4562@gmail.com>
2023-10-03 00:09:21 +0900

    deleted

--- dilly_ws/src/dilly_cartographer/dilly_lua/dilly_lua.lua
+++ dilly_ws/src/dilly_cartographer/dilly_lua/dilly_lua.lua
@@ -52,7 +52,5 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching=true
 TRAJECTORY_BUILDER_2D.use_imu_data = true
-TRAJECTORY_BUILDER_2D.
-
 
 return options
